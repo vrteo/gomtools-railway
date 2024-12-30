01eb4f81cb29e3b62ce5bbc2b8d45c9e8ee6ea63
Teo <dianateodorav@gmail.com>
2024-12-30 23:21:38 +0200

    Fix incorrect mappedBy attribute in GroupModel annotations
    Updated the mappedBy attribute for the groupOrder list to "goItems" to match the correct field in the associated entity. This ensures proper JPA mapping and prevents potential runtime errors.

--- src/main/java/com/fixontricky/gomtools/model/GroupModel.java
+++ src/main/java/com/fixontricky/gomtools/model/GroupModel.java
@@ -19,6 +19,6 @@ public class GroupModel {
     @OneToMany(mappedBy = "group")
     private List<MemberModel> members = new ArrayList<>();
 
-    @OneToMany(mappedBy = "goItems")
+    @OneToMany(mappedBy = "group")
     private List<GroupOrderModel> groupOrder;
 }
